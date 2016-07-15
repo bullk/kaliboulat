d2fe3721e0c6d1c2d08ab7d3078689b11309ed26
Al Kali Boul <olivier@boulocheland.net>
2016-07-15 16:41:32 +0200

    Fixed preceding mistake (Kaliboulat <> Kaliboulat.cpp)

--- Kaliboulat.cpp
+++ Kaliboulat.cpp
@@ -159,20 +159,24 @@ int main( int argc, char* args[] )
             ImGui::Text("Hello");
 			// Use functions to generate output
 			// FIXME: This is rather awkward because current plot API only pass in indices. We probably want an API passing floats and user provide sample rate/count.
-			struct Funcs
-			{
-				static float Sin(void*, int i) { return sinf(i * 0.1f); }
-				static float Saw(void*, int i) { return (i & 1) ? 1.0f : 0.0f; }
-			};
-			static int func_type = 0, display_count = 70;
-			ImGui::Separator();
-			ImGui::PushItemWidth(100); ImGui::Combo("func", &func_type, "Sin\0Saw\0"); ImGui::PopItemWidth();
-			ImGui::SameLine();
-			ImGui::SliderInt("Sample count", &display_count, 1, 500);
-			float (*func)(void*, int) = (func_type == 0) ? Funcs::Sin : Funcs::Saw;
-			ImGui::PlotLines("Lines", func, NULL, display_count, 0, NULL, -1.0f, 1.0f, ImVec2(0,80));
-			ImGui::PlotHistogram("Histogram", func, NULL, display_count, 0, NULL, -1.0f, 1.0f, ImVec2(0,80));
-			ImGui::Separator();
+			//struct Funcs
+			//{
+				//static float Sin(void*, int i) { return sinf(i * 0.1f); }
+				//static float Saw(void*, int i) { return (i & 1) ? 1.0f : 0.0f; }
+			//};
+			//static int func_type = 0, display_count = 70;
+			//ImGui::Separator();
+			//ImGui::PushItemWidth(100); ImGui::Combo("func", &func_type, "Sin\0Saw\0"); ImGui::PopItemWidth();
+			//ImGui::SameLine();
+			//ImGui::SliderInt("Sample count", &display_count, 1, 500);
+			//float (*func)(void*, int) = (func_type == 0) ? Funcs::Sin : Funcs::Saw;
+			//ImGui::PlotLines("Lines", func, NULL, display_count, 0, NULL, -1.0f, 1.0f, ImVec2(0,80));
+			//ImGui::PlotHistogram("Histogram", func, NULL, display_count, 0, NULL, -1.0f, 1.0f, ImVec2(0,80));
+			//ImGui::Separator();
+			static float progress = 0.5f;
+			char buf[32];
+			sprintf(buf, "%d/%d", (int)(progress_saturated*1753), 1753);
+			ImGui::ProgressBar(progress, ImVec2(0.f,0.f), buf);
             ImGui::End();
         }
 
