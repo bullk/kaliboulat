93c5c86fb6c3e8369aacaa1d2184aeafa91e5984
Al Kali Boul <olivier@boulocheland.net>
2017-03-12 20:57:51 +0100

    wavrepair upgrade

--- wavrepair.cpp
+++ wavrepair.cpp
@@ -1,23 +1,56 @@
+#include <string>
+#include <vector>
+#include <cmath>
 #include <stk/FileWvIn.h>
 #include <stk/FileWvOut.h>
 
-#define FILEIN "/home/jeandamme/jack_capture_01.wav"
-#define FILEOUT "/home/jeandamme/repaired.wav"
-//#define FILEIN "/home/jeandamme/white.aiff"
+#define FILEIN "/home/bullk/test_06.wav"
 
 int main( int argc, char* args[] )
 {
-	stk::FileWvIn filewvin = stk::FileWvIn( FILEIN, true );
-	stk::FileWvOut filewvout = stk::FileWvOut( FILEOUT );
-	std::cout << "channels : " << filewvin.channelsOut() << std::endl;
-	for ( unsigned int i=0; i<1; i++ )
+	stk::FileWvIn filewvin = stk::FileWvIn( FILEIN, false, true );
+	for ( unsigned int i=0; i<filewvin.channelsOut(); i++ )
 	{
+		std::string fout = FILEIN + std::string("-") + std::to_string(i);
+		stk::FileWvOut filewvout = stk::FileWvOut( fout );
+		std::cout << "channel " << i << std::endl;
+		std::vector<stk::StkFloat> data;
+		int mod = 0;
+		float delta = 0.0f;
+		float max = 0.0f;
+		float corrected;
 		filewvin.reset();
-		stk::StkFloat previous = filewvin.tick(i);
-		filewvout.tick(previous);
-		filewvin.tick(i);
+		stk::StkFloat previous;
+		stk::StkFloat value = filewvin.tick(i);
+		//~ filewvout.tick( previous );
+		while ( not( filewvin.isFinished() ) )
+		{
+			//~ std::cout << "value : " << value << std::endl;
+			corrected = value + mod;
+			data.push_back( corrected );
+			if ( std::abs(corrected) > max ) max = std::abs(corrected);
+			previous = value;
+			value = filewvin.tick(i);
+			delta = value - previous;
+			if ( delta > 1 )
+			{
+				if ( mod == 0 ) mod = -2;
+				else mod = 0;
+				std::cout << "delta : " << delta << std::endl;
+			}
+			else if ( delta < -1 )
+			{
+				if ( mod == 0 ) mod = 2;
+				else mod = 0;
+				std::cout << "delta : " << delta << std::endl;
+			}
+			
+		}
+		std::cout << "maximum amp : " << max << std::endl;
+		for ( std::vector<stk::StkFloat>::iterator f=data.begin(); f != data.end(); f++ )
+			filewvout.tick( *f / max );
+		filewvout.closeFile();
 	}
 	filewvin.closeFile();
-	filewvout.closeFile();
 	return 0;
 }
